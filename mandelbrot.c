b5df45edd48310ed1ed6cae14959ccf4e9ca4ab6
thomasscottcrawford20@gmail.com <thomasscottcrawford20@gmail.com>
2026-01-04 12:41:14 -0600

    Put the point recursion in it's own function

--- mandelbrot.c
+++ mandelbrot.c
@@ -4,8 +4,6 @@
 #include <math.h> // sin, sqrt, pow, etc
 
 
-
-
 typedef struct c32 {
 	float r, i;
 } c32;
@@ -26,13 +24,24 @@ float map32(float Value, float ValMin, float ValMax, float MappedMin, float Mapp
 	float _scale = (MappedMax - MappedMin) / (ValMax - ValMin);
 	return MappedMin + _scale * (Value - ValMin);
 }
+c32 recursePoint(int *Iteration, int _iterations, c32 Position) {
+	c32 PositionDynamic = Position;
+	while (++(*Iteration) < _iterations) {
+		//z→z^2+c
+		PositionDynamic = c32_add(c32_mul(PositionDynamic, PositionDynamic), Position);
+		if (c32_abs(PositionDynamic) > 2) {
+			break;
+		}
+	}
+	return PositionDynamic;
+}
 
 float Width = 600, Height = 400;
-int Iterations = 100;
 
 int main() {
 	float OffsetX = 0.f, OffsetY = 0.f;
 	float Scale = 1;
+	int Iterations = 100;
 	Vector2 Position = {-0.3f, 0.f};
 	InitWindow((int)Width, (int)Height, "mandelbrot set");
 	if (Width > Height) { // fixes the center in the middle of the screen, even if viewed from a rectangular screen
@@ -49,25 +58,20 @@ int main() {
 				float x = map32(i, 0.0 + OffsetX, Win + OffsetX, Position.x - Scale, Position.x + Scale);
 				float y = map32(j, 0.0 + OffsetY, Win + OffsetY, Position.y - Scale, Position.y + Scale);
 				c32 Point = { x, y };
-				c32 ChangingPt = Point;
 				int Iteration = 0;
-				while (++Iteration < Iterations) {
-					//z→z^2+c
-					ChangingPt = c32_add(c32_mul(ChangingPt, ChangingPt), Point);
-					if (c32_abs(ChangingPt) > 2) {
-						break;
-					}
-				}
-				if (c32_abs(ChangingPt) < 2) {
+				recursePoint(&Iteration, Iterations, Point);
+
+				if (Iteration == Iterations) {
 					DrawPixel(i, j, BLACK);
-				} else {
+				}
+				else {
 					float scaled = 255 / Iterations * Iteration;
 					DrawPixel(i, j, ColorFromHSV(scaled, 255, 255));
 				}
 			}
 		}
 		
-		DrawRectangle(0, 0, 20, 20, BLUE);
+		DrawFPS(10, 10);
 		EndDrawing();
 		
 	}
